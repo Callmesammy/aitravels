440a0c94c60fbb39f77d8fa683e6b890ec9d8a11
Callmesammy <callmesammy@yahoo.com>
2025-04-30 06:38:20 -0700

    update

---
+++ app/(root)/_components/index.ts
@@ -0,0 +1,63 @@
+export const addItems = [
+    {
+        id: 1,
+        title: "Total Users", 
+        amount: 120000,
+        percent: "12%",
+        perios: "vs last month"
+    },
+    {
+        id: 2,
+        title: "Total Trips", 
+        amount: 11000,
+        percent: "2%",
+
+        perios: "vs last month"
+    },
+    {
+        id: 3,
+        title: "Active Users Today", 
+        amount: 800,
+        percent: "3.3%",
+        perios: "vs last month"
+    },
+]
+
+export const addtravels = [
+    {
+        id: 1, 
+        amount: 250,
+        img: "/1.jpg", 
+        place: "Cape Town Beach", 
+        map: "11a Drive Captown South Africa", 
+        tour: "Mountains",
+        features: "city",
+    },
+    {
+        id: 2, 
+        amount: 350,
+        img: "/2.webp", 
+        place: "Roraima Tepui", 
+        map: "Canaima Park, Venezuela", 
+        tour: "Solo Travel",
+        features: "Budget",
+    },
+    {
+        id: 3, 
+        amount: 200,
+        img: "/3.webp", 
+        place: "Socotra Island", 
+        map: "Yemen", 
+        tour: "Luxury",
+        features: "Beach",
+    },
+    {
+        id: 4, 
+        amount: 500,
+        img: "/4.avif", 
+        place: "San Lake Baikal", 
+        map: "Siberia, Rusia", 
+        tour: "Sports",
+        features: "Adventurous",
+    },
+]
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 import { Button } from "@/components/ui/button";
 import { createClient } from "@/utils/supabase/client";
-import { Plus } from "lucide-react";
+import { ArrowDown, ArrowUp, MapPin, Plus } from "lucide-react";
 import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
 import { MdOutlineWavingHand } from "react-icons/md";
+import { addItems, addtravels } from "./_components";
+import Image from "next/image";
 
 const Home = () =>{
 const [use, setUse] = useState()
@@ -22,7 +24,7 @@ const [use, setUse] = useState()
 
 }
   return (
-    <div className="flex h-full w-full px-2 pt-2 flex-col ">
+    <div className="flex h-full w-full px-2 pt-2 flex-col overflow-auto mb-2 ">
       <div className="justify-between w-full flex items-start">
       <div className="flex flex-col">
        <h1 className="text-md font-semibold flex items-center gap-1 capitalize">Welcome {use} <MdOutlineWavingHand />
@@ -31,9 +33,44 @@ const [use, setUse] = useState()
        </div> 
        <Button variant="outline" className="bg-sky-800 text-white cursor-pointer"><Plus/> Create a trip</Button>
       </div>
-      <div className=" mt-3 grid md:grid-cols-2 lg:grid-cols-3 w-full rounded-md h-42  items-start  ">
-        {}
-     ff
+      <div className=" mt-3 grid md:grid-cols-2 lg:grid-cols-3 w-full lg:h-32 rounded-md h-full  gap-2  items-start  ">
+        {addItems.map((item)=>(
+          <div key={item.id} className="w-full h-32 rounded  border px-3 pt-3 items-center space-y-2 ">
+            <h1 className="text-sm ">{item.title} </h1>
+           <div className="flex justify-between"> <h1 className="text-3xl">{item.amount}</h1> {item.id === 2? ( <span className="bg-red-700 items-center text-white justify-center text-xm p-2">Trips</span>):(<span className="bg-green-800 items-center text-white justify-center text-xm p-2">Profit</span>)}</div> 
+            <div>{item.percent.length >= 3 ? (<div className="text-green-800 font-semibold flex gap-1 text-sm"><ArrowUp className="size-5"/> {item.percent} <p className="text-muted-foreground">{item.perios}</p>  </div>):(<div className="text-red-800 font-semibold flex text-sm gap-0"> <ArrowDown className="size-5"/> {item.percent}<p className="text-muted-foreground">{item.perios}</p></div>)}</div>
+          </div>
+        ))}
+      </div>
+      <div className="pt-5 flex flex-col w-full h-full ">
+      <h1 className="text-md font-bold">Trips</h1>
+      <div className="grid md:grid-cols-2  lg:grid-cols-4 w-full h-full gap-2">
+          {addtravels.map((addT)=>{
+
+            return(
+            <div key={addT.id} className="border flex flex-col rounded-md w-full h-[16rem]">
+              <div className="w-full h-[10rem] border rounded-t-md relative">
+                <Image src={addT.img} alt="image" fill className="object-fill rounded-t-md"/>
+                <div className="rounded end-2 mt-2 absolute px-3  bg-white text-black text-center text-sm items-center flex font-semibold">$ {addT.amount}</div>
+              </div>
+              <div className="px-2 pt-1 font-semibold text-purple-950">{addT.place}</div>
+              <span className="text-sm px-2 pt-2 text-muted-foreground flex gap-1 items-center"><MapPin className="size-5"/> {addT.map}</span>
+              <span className="text-sm px-2  flex gap-1 items-center">
+              <span className="text-sm px-2 bg-green-200 rounded-lg text-center mt-2 text-muted-foreground flex gap-1 items-center">
+                {addT.tour}
+                </span>
+                <span className="text-sm px-2 bg-purple-200 rounded-lg text-center mt-2 text-muted-foreground flex gap-1 items-center">
+                {addT.features}
+                </span>
+               </span>
+
+            </div>  
+            )
+          }
+            
+          )}
+
+      </div>
       </div> 
     </div>
   );
