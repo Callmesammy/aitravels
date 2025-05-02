d0d6d1bc60941ca9794b4b68a66c5d0f82e3f524
Callmesammy <callmesammy@yahoo.com>
2025-05-02 08:46:20 -0700

    update

--- app/(root)/trips/[id]/page.tsx
+++ app/(root)/trips/[id]/page.tsx
@@ -1,6 +1,61 @@
+"use client"
+
+import { createClient } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
+
+interface details {
+  id: number,
+  taskDetails: {
+      name: string,
+      estimatedPrice: string,
+      duration: number, 
+      budget: string
+
+  }
+}
+
 
 const TripDetailsPage = ({ params }: { params: { id: string } }) => {
-    return <div>Trip ID: {params.id}</div>;
+
+  const [listing, Setlisting] = useState<details[]>()
+
+
+  useEffect(()=>{
+    itemsList()
+  },[])
+
+  const itemsList = async()=>{
+    const supabase = await createClient()
+    const{data, error} = await supabase.from("upload").select("*")
+    if (data) {
+      const parsedData = data.map((doc) => ({
+        ...doc,
+        taskDetails:
+          typeof doc.taskDetails === "string"
+            ? JSON.parse(doc.taskDetails)
+            : doc.taskDetails,
+      }));
+  
+      Setlisting(parsedData as details[]);
+    } else {
+      console.log(error);
+    }
+
+  }
+
+    return <div className="w-full h-full flex pt-3 px-2 "> 
+    <div className="w-full h-full flex flex-col ">
+      <h1 className="font-semibold ">Trips</h1>
+    <p className="text-muted-foreground text-sm">View and edit Ai generated travel plans</p>
+    </div>
+    <div>
+      {listing?.map((doc)=>(
+        <div key={doc.id} className="font-bold text-black text-2xl">
+          {doc.taskDetails.name}
+        </div>
+      ))}
+    </div>
+    </div>;
   };
   
   export default TripDetailsPage;
